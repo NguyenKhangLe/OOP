55eef44ea62853fd0b6eceed38d584ff4b9100df
KhangLe <88606778+NguyenKhangLe@users.noreply.github.com>
2023-02-07 22:26:50 +0700

    Add files via upload

---
+++ 2.1P/CounterClass/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+namespace CounterClass
+{
+    class MainClass
+    {
+        private static void PrintCounters(Counter[] counters)
+        {
+            foreach (Counter c in counters)
+            {
+                Console.WriteLine($"{c.Name} is {c.Ticks}");
+            }
+        }
+        public static void Main(string[] args)
+        {
+            Counter[] myCounters = new Counter[3];
+            int i;
+
+            myCounters[0] = new Counter("Counter 1");
+            myCounters[1] = new Counter("Counter 2");
+            myCounters[2] = myCounters[0];
+
+            for (i = 0; i <= 9; i++)
+            {
+                myCounters[0].Increment();
+            }
+
+            for (i=1; i <= 14; i++)
+            {
+                myCounters[1].Increment();
+            }
+
+            PrintCounters(myCounters);
+
+            myCounters[2].Reset();
+
+            PrintCounters(myCounters);
+
+        }
+    }
+}
