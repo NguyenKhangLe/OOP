02cef8092c1b7454c93b5c6260efd76f92d9a405
KhangLe <88606778+NguyenKhangLe@users.noreply.github.com>
2023-02-07 22:21:54 +0700

    Add files via upload

---
+++ 1.2P/1.2P/Program.cs
@@ -0,0 +1,47 @@
+﻿using HelloWorld;
+using System;
+
+namespace HelloWord
+{
+    class MainClass
+    {
+        static void Main(string[] args)
+        {
+            var myMessage = new Message("Hello World - from Message object");
+            myMessage.Print();
+
+            Message[] messages = new Message[5];
+            messages[0] = new Message("Welcome back!");
+            messages[1] = new Message("What a lovely name");
+            messages[2] = new Message("Great name");
+            messages[3] = new Message("Oh hi!");
+            messages[4] = new Message("That is a silly name");
+
+            Console.Write("Enter the name: ");
+            string name = Console.ReadLine();
+
+            if (name.ToLower() == "phuong")
+            {
+                messages[0].Print();
+            }
+
+            else if (name.ToLower() == "messi")
+            {
+                messages[1].Print();
+            }
+
+            else if (name.ToLower() == "ronaldo")
+            {
+                messages[2].Print();
+            }
+
+            else if (name.ToLower() == "neymar")
+            {
+                messages[3].Print();
+            }
+
+            else
+                messages[4].Print();
+        }
+    }
+}
